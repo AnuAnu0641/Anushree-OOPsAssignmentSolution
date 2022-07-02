3e5a5b70301d14ef3d6bafec52728a3c38cfc1b8
AnuAnu0641 <104294736+AnuAnu0641@users.noreply.github.com>
2022-07-02 21:26:12 +0530

    Add files via upload

---
+++ DepartmentsDailyUpdates/src/com/department/types/HrDepartment.java
@@ -0,0 +1,26 @@
+package com.department.types;
+
+import com.department.parent.SuperDepartment;
+
+public class HrDepartment extends SuperDepartment{
+
+	@Override
+	public String departmentName() {
+		return "Hr Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		return "Fill today's worksheet and mark your attendance";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public String doActivity() {
+		return "Team Lunch";
+	}
+	
+}
---
+++ DepartmentsDailyUpdates/src/com/department/types/TechDepartment.java
@@ -0,0 +1,26 @@
+package com.department.types;
+
+import com.department.parent.SuperDepartment;
+
+public class TechDepartment extends SuperDepartment {
+
+	@Override
+	public String departmentName() {
+		return "Tech Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		return "Complete coding of module 1";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String getTechStackInformation() {
+		return "Core Java";
+	}
+	
+}
